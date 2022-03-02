1e831c6144759a8c974e5e377a22900a0f93f8f6
Bike2400 <ebukaechezona8@gmail.com>
2022-03-02 22:37:36 +0100

    Added the computerPlay function

---
+++ script.js
@@ -0,0 +1,10 @@
+function computerPlay(){
+let arr = ['Rock','Paper','Scissors'];
+return (arr[Math.floor(Math.random() * arr.length)]);
+}
+console.log(computerPlay());
+
+function playRound(playerSelection,computerSelection){
+
+}
+
