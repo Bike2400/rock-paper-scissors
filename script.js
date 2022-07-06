7a25cf89d37a703d1a04099a156d1a98b871ca4b
Bike2400 <ebukaechezona8@gmail.com>
2022-07-06 21:35:04 +0100

    Edited the Ui so that the moveslist does not show

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
-const game = () => {
+const game = () =>{
     let playerScore = 0;
     let computerScore = 0;
-    let moves = 0;
- 
- 
-    const playGame = () => {
-        const options = document.querySelectorAll('.options')
+
+    const playGame = () =>{
+        const options = document.querySelectorAll('.options');
         const computerOptions = ['rock','paper','scissors']
-         
-        // Function to start playing game
-        options.forEach(option => {
+
+        options.forEach(option =>{
             option.addEventListener('click',function(){
-                const pInput = this.textContent;
- 
-                const movesLeft = document.querySelector('.movesleft');
-                moves++;
-                movesLeft.innerText = `Moves Left: ${10-moves}`;
-                 
- 
+                const pInput = this.textContent; // So that the player input will be the value of whatever is in the 'options' tag that is clicked
+
                 const choiceNumber = Math.floor(Math.random()*3);
                 const computerChoice = computerOptions[choiceNumber];
- 
-                // Function to check who wins
-                winner(this.innerText,computerChoice)
-                 
-                // Calling gameOver function after 10 moves
-                if(playerScore ==5 || computerScore == 5){
-                    gameOver(options,movesLeft);
+
+                winner(this.innerText,computerChoice);
+
+                if(playerScore == 5|| computerScore ==5){
+                    gameOver(options);
                 }
             })
         })
-         
     }
- 
-    // Function to decide winner
+
+    // Function to check who wins
     const winner = (player,computer) => {
         const result = document.querySelector('.result');
         const playerScoreBoard = document.querySelector('.p-count');
@@ -41,26 +30,15 @@ const game = () => {
         player = player.toLowerCase();
         computer = computer.toLowerCase();
         if(player === computer){
-            result.textContent = 'Tie'
+            result.textContent = 'Draw'
         }
         else if(player == 'rock'){
             if(computer == 'paper'){
                 result.textContent = 'Computer Won';
                 computerScore++;
                 computerScoreBoard.textContent = computerScore;
- 
-            }else{
-                result.textContent = 'Player Won'
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
             }
-        }
-        else if(player == 'scissors'){
-            if(computer == 'rock'){
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            }else{
+            else{
                 result.textContent = 'Player Won';
                 playerScore++;
                 playerScoreBoard.textContent = playerScore;
@@ -71,56 +49,51 @@ const game = () => {
                 result.textContent = 'Computer Won';
                 computerScore++;
                 computerScoreBoard.textContent = computerScore;
-            }else{
+            }
+            else{
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent= playerScore;
+            }
+        }
+        else if(player == 'scissors'){
+            if(computer == 'rock'){
+                result.textContent = 'Computer Won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+            }
+            else{
                 result.textContent = 'Player Won';
                 playerScore++;
                 playerScoreBoard.textContent = playerScore;
             }
         }
     }
- 
-    // Function to run when game is over
-    const gameOver = (options,movesLeft) => {
- 
-        const chooseMove = document.querySelector('.move');
+    const gameOver = (options) =>{
         const result = document.querySelector('.result');
         const reloadBtn = document.querySelector('.reload');
- 
-        options.forEach(option => {
+
+        options.forEach(option =>{
             option.style.display = 'none';
-        })
- 
-      
-        chooseMove.innerText = 'Game Over!!'
-        movesLeft.style.display = 'none';
- 
+        });
+
         if(playerScore > computerScore){
-            result.style.fontSize = '2rem';
-            result.innerText = 'You Won The Game'
-            result.style.color = '#308D46';
-        }
-        else if(playerScore < computerScore){
-            result.style.fontSize = '2rem';
-            result.innerText = 'You Lost The Game';
-            result.style.color = 'red';
+            result.style.fontSize = '32px';
+            result.innerText = 'Yayy you Won!'
+            result.style.color = 'green';
+            reloadBtn.style.backgroundColor = 'green';
         }
         else{
-            result.style.fontSize = '2rem';
-            result.innerText = 'Tie';
-            result.style.color = 'grey'
+            result.style.fontSize = '32px';
+            result.innerText = 'Too Bad you lost'
+            result.style.color = 'red';
         }
-        reloadBtn.innerText = 'Restart';
-        reloadBtn.style.display = 'flex'
+        reloadBtn.innerText = 'Restart Game';
+        reloadBtn.style.display = 'flex';
         reloadBtn.addEventListener('click',() => {
             window.location.reload();
         })
     }
- 
- 
-    // Calling playGame function inside game
     playGame();
-     
-    
 }
-// Calling the game function
 game();
